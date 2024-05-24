e7304d37735d47e3587d0899be04260979c34529
Hannah6898 <hannahosibodu68@outlook.com>
2024-05-24 14:50:00 +0100

    Initial commit

---
+++ src/main/java/com/ecommerceApp/JavaBackend/Controllers/LoginController.java
@@ -0,0 +1,13 @@
+package com.ecommerceApp.JavaBackend.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping(path = "login")
+    public String login(){
+        return "login";
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -0,0 +1,6 @@
+<!-- mypage.jsp -->
+<html>
+<body>
+    <h1>Hello, this is a JSP page!</h1>
+</body>
+</html>
